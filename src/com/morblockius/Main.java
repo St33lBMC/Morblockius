a13b1b27cbb6119036bd2007bf5e3c3d70bea480
Exopteron <exopteron@protonmail.com>
2023-09-16 11:17:05 +1200

    hello world window

--- src/com/morblockius/Main.java
+++ src/com/morblockius/Main.java
@@ -1,8 +1,20 @@
 package com.morblockius;
 
 import org.lwjgl.glfw.*;
-public class Main {
+import java.awt.*;
+
+public class Main  {
     public static void main(String[] args) {
+        Frame f = new Frame();
+
+        Label l = (Label) f.add(new Label("Hello World"));
+
+        l.setAlignment(Label.CENTER);
+
+        f.setBounds(0, 0, 100, 100);
+
+        f.setVisible(true);
+        
         System.out.println("Hello World");
     }
 }
